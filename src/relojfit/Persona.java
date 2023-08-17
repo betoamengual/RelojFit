257d182ac39cd36c125929e93bd8e98f042bcd0e
matgithubulp <ejarquegithub@gmail.com>
2023-08-17 17:46:33 -0300

    ClasePersona
    Falta agregar el metodo comer y hablar.

--- src/relojfit/Persona.java
+++ src/relojfit/Persona.java
@@ -10,4 +10,47 @@ package relojfit;
  */
 public class Persona {
     
+    private String nombre, apellido;
+    private float altura;
+    private int edad;
+    private Reloj reloj;
+
+    public Persona(String nombre, String apellido, float altura, int edad, Reloj reloj) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.altura = altura;
+        this.edad = edad;
+        this.reloj = null;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+    
+    
+     public void asociarReloj(Reloj reloj) {
+        this.reloj = reloj;
+    }
+     
+     public void verHora() {
+        if (reloj != null) {
+            System.out.print(nombre + " " + apellido + " mira el reloj. ");
+            reloj.getHora();
+        } else {
+            System.out.println(nombre + " " + apellido + " no tiene un reloj asociado.");
+        }
+    }
+
 }
