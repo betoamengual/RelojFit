77c8204cbdbd70c82767a832c37b417fad5f78b0
JuanCarrillo2435 <juancarrillo.dc@gmail.com>
2023-08-17 15:20:03 -0300

    Y esta la clase reloj con los metodos de incrementar dia y hora.

--- src/relojfit/RelojFit.java
+++ src/relojfit/RelojFit.java
@@ -15,7 +15,9 @@ public class RelojFit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Reloj r1 = new Reloj("2023-08-17","13:30");
+        //r1.incremenetarDia();
+        //r1.incrementarHora();
     }
     
 }
